c4ee905f6e34ce4747f2b5707af501d8865b2d71
tomeullompartpiza <tomeullompartpiza@gmail.com>
2026-01-01 22:02:29 +0100

    accessing db model fields

--- yubel.rb
+++ yubel.rb
@@ -1,4 +1,6 @@
 
+require_relative "config/environment"
+
 # Look for classes that extend ApplicationRecord
 def find_ruby_files_in_path(path)
   model_files = []
@@ -11,6 +13,13 @@ def find_ruby_files_in_path(path)
   model_files
 end
 
+def extract_application_record_name(string)
+  # first remove the the < ApplicationRecord
+  klass, _ = string.split("<")
+  # then remove the class
+  _, model_name = klass.split("class")
+  model_name
+end
 
 def scan_app
   path = "app/models"
@@ -23,13 +32,19 @@ def scan_app
     File.open(file, "r") do |lines|
       lines.each do |line|
         if line.include? needle
-          application_records << file
+          name = extract_application_record_name(line)
+          application_records << name
         end
       end
     end
   end
 
-  puts application_records
+  application_records.each do |model|
+    model_name = model.strip
+    puts "Getting fields of #{model_name}"
+    column_names = "#{model_name.strip}.column_names"
+    system("rails runner 'puts #{column_names}'")
+  end
 end
 
 scan_app
